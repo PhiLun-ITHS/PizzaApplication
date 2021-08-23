8a37c3b8e756ed001e6d2bf43645835139dece06
PL <philip.lundin@iths.se>
2021-08-23 22:09:08 +0200

    added tests

--- src/test/java/com/example/PizzaApplication/controllers/PizzaMVCTest.java
+++ src/test/java/com/example/PizzaApplication/controllers/PizzaMVCTest.java
@@ -1,13 +1,16 @@
 package com.example.PizzaApplication.controllers;
 
+import com.example.PizzaApplication.entities.Pizza;
+import com.example.PizzaApplication.repositories.PizzaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = PizzaController.class)
@@ -16,9 +19,32 @@ class PizzaMVCTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PizzaRepository pizzaRepository;
+
     @Test
-    void getAllPizzasReturnsOnePizza() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/1"))
+    void getAllPizzasReturnsAllPizzas() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/pizzas"))
                 .andExpect(status().is(200));
     }
+
+    @Test
+    void addPizza() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(new Pizza(10L, "Vesuvio", "Tomatsås, Ost, Skinka", 80)))
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isCreated());
+
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
